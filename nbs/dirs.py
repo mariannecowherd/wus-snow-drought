2dd13aa9dca92562838b0e971cf3a2d2456c084d
Marianne <mariannecowherd@gmail.com>
2025-10-14 16:02:00 -0600

    add dirs

---
+++ nbs/dirs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+
+## define directories
+## base directory
+basedir = '/glade/u/home/mcowherd/snow_drought/downscaling/'
+## project 
+projectdir = '/glade/campaign/uwyo/wyom0200/'
+## downscaled GCM data
+wrfdir = '/glade/campaign/uwyo/wyom0200/wus-d3/postprocess/'
+## downscaled GCM metadata
+coorddir = wrfdir + 'meta/meta_new/'
+## save big outs
+savedir = '/glade/campaign/uwyo/wyom0200/berkeley/'
+## working dir
+workdir = '/glade/work/mcowherd/'
